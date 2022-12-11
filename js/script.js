9051dbfc023cbeb62a46e7ee301948adc98b1b8a
PracticeBES <igor-lebedev-02@mail.ru>
2022-12-11 22:42:45 +0500

    test

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const a = document.querySelectorAll("a");
-const slice = 22;
-for (let i = 0; i < a.length; i++) {
-    if (a[i].textContent.length > slice) {
-        let text = a[i].textContent.slice(0, slice) + "...";
-        a[i].innerText = text;
-    }
-}
